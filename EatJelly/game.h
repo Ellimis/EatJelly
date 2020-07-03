4985a91dc69268db37059c0a7557622f1308dd5e
Ellimis <Ellimis@github.com>
2020-07-03 10:19:07 +0900

    add game.h for management

---
+++ EatJelly/game.h
@@ -0,0 +1,14 @@
+#include "gameobject.h"
+
+class GameManager {
+	char map[10][20] = { '-' }; // x : 0~9, y : 0~19
+public:
+	GameManager() {
+		for (int x = 0; x < 10; x++)
+			for (int y = 0; y < 20; y++)
+				map[x][y] = '-';
+	};
+	void drawMap();
+	void moveAll();
+	bool isCollide();
+};
